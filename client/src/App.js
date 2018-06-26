917f73832a9fbc5d4518e6288a61fede6b97e724
Dwalton55 <deonte.e.walton@gmail.com>
2018-06-26 09:50:49 -0400

    beginning work in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,24 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import { Link, Switch, BrowserRouter as Router, Route } from 'react-router-dom'
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  state = {
+      games : []
+  }
+
+  componentDidMount () {
+   
+    axios.get('/api/games').then((res) => {
+      this.setState({ games: res.data.games })
+    }).catch((err) => {
+      console.error(err)
+    })
+  }
+
+
   render() {
     return (
       <div className="App">
